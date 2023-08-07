3f3f4d95a240c5c6a907b13dbd1501027c505a9c
Sooner359 <123106109+Sooner359@users.noreply.github.com>
2023-08-07 10:00:44 -0500

    Add grid size button
    Added a button to change grid size. Also changed the reset button to clear the board by removing the class instead of reloading the page.

--- etch.js
+++ etch.js
@@ -1,26 +1,35 @@
 const container = document.querySelector('#container');
 
-function createGrid (n) {
-    //n=prompt("Choose a grid size:")
-    for (let i = 0; i<(n); i++) {
+function createGrid(n) {
+    
+    for (let i = 0; i < (n); i++) {
         const row = document.createElement('div');
         container.appendChild(row);
 
-        for (let j = 0; j<n; j++) {
+        for (let j = 0; j < n; j++) {
             const square = document.createElement('div')
             square.classList.add('grid')
             row.appendChild(square)
-            square.style.width=(500/n) + "px"
-            square.style.height=(500/n) + "px"
-            
-            square.addEventListener('mouseover', function() {square.classList.add("hover")})
+            square.style.width = (500 / n) + "px"
+            square.style.height = (500 / n) + "px"
+
+            square.addEventListener('mouseover', function () { square.classList.add("hover") })
+            function removeGrid() {
+    
+                square.classList.remove("hover")
+            }
+
+            const resetBtn = document.querySelector('#reset-btn')
+            resetBtn.addEventListener('click', function () { removeGrid() })
+
         }
     }
 }
 
 
-createGrid(prompt("Choose a grid size. Example:16 would be 16X16"))
 
-function removeGrid (){
-    window.location.reload()
-}
+const gridSize = document.querySelector('#grid-size')
+gridSize.addEventListener('click', function(){createGrid(parseInt(prompt("Choose", 16)))})
+
+
+
