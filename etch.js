ede96fcefda9b65767692c25cead323c87836560
Sooner359 <123106109+Sooner359@users.noreply.github.com>
2023-08-08 12:52:51 -0500

    Fix grid size button
    Fixed the grid size button so it correctly replaces the current grid instead of placing a new grid below the existing grid.
    CHanged the size and color of the buttons.

--- etch.js
+++ etch.js
@@ -1,35 +1,47 @@
 const container = document.querySelector('#container');
 
 function createGrid(n) {
-    
-    for (let i = 0; i < (n); i++) {
-        const row = document.createElement('div');
-        container.appendChild(row);
+    if (n<101) {
+        for (let i = 0; i < (n); i++) {
+            const row = document.createElement('div');
+            container.appendChild(row);
 
-        for (let j = 0; j < n; j++) {
-            const square = document.createElement('div')
-            square.classList.add('grid')
-            row.appendChild(square)
-            square.style.width = (500 / n) + "px"
-            square.style.height = (500 / n) + "px"
+            for (let j = 0; j < n; j++) {
+                const square = document.createElement('div')
+                square.classList.add('grid')
+                row.appendChild(square)
+                square.style.width = (500 / n) + "px"
+                square.style.height = (500 / n) + "px"
 
-            square.addEventListener('mouseover', function () { square.classList.add("hover") })
-            function removeGrid() {
-    
-                square.classList.remove("hover")
+                square.addEventListener('mouseover', function () { square.classList.add("hover") })
+                function removeGrid() {
+        
+                    square.classList.remove("hover")
+                }
+
+                const resetBtn = document.querySelector('#reset-btn')
+                resetBtn.addEventListener('click', function () { removeGrid() })
+
             }
-
-            const resetBtn = document.querySelector('#reset-btn')
-            resetBtn.addEventListener('click', function () { removeGrid() })
-
         }
+    }
+
+    else {
+        return (createGrid(16), alert("Max grid size is 100"))
     }
 }
 
+createGrid(16)
+
+function deleteGrid () {
+    const squares = document.querySelectorAll(".grid")
+    squares.forEach(square => {
+        square.remove()
+    });
+}
 
 
 const gridSize = document.querySelector('#grid-size')
-gridSize.addEventListener('click', function(){createGrid(parseInt(prompt("Choose", 16)))})
-
+gridSize.addEventListener('click', function(){deleteGrid(), createGrid(prompt("Choose", 16))})
 
 
